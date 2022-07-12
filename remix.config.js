dd46583f897b81d9823bb6aeb90231dd7ede0d2c
amilin <moh.amilin@moladin.com>
2022-07-12 21:54:14 +0700

    cancel netlify

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: "netlify",     // for netlify
-  server: "./server.js",            // for netlify
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
